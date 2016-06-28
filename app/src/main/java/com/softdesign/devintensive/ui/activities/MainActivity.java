06bd7d164ea7abd0c1ac0d3a909cd394f37d1a63
Mihail Reznichenko <mihail.reznichenko.98@mail.ru>
2016-06-28 15:56:30 +0600

    add ToolBar, add HomeButton, update NavigationDrawer

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -2,10 +2,14 @@ package com.softdesign.devintensive.ui.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -17,6 +21,8 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
 
     private ImageView mCallImg;
     private CoordinatorLayout mCoordinatorLayout;
+    private Toolbar mToolbar;
+    private DrawerLayout mNavigationDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +32,12 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
 
         mCallImg = (ImageView)findViewById(R.id.call_img);
         mCoordinatorLayout = (CoordinatorLayout)findViewById(R.id.main_coordinator_container);
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        mNavigationDrawer = (DrawerLayout)findViewById(R.id.navigation_drawer);
 
         mCallImg.setOnClickListener(this);
+        setupToolbar();
+        setupDrawer();
 
         if (savedInstanceState == null){
             //активити запускается впервые
@@ -75,6 +85,14 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            mNavigationDrawer.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.call_img:
@@ -85,5 +103,27 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
 
     private void showSnackBar(String message){
         Snackbar.make(mCoordinatorLayout, message, Snackbar.LENGTH_LONG).show();
+    }
+
+    private void setupToolbar(){
+        setSupportActionBar(mToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    private void setupDrawer(){
+        NavigationView navigationView = (NavigationView)findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                showSnackBar(item.getTitle().toString());
+                item.setChecked(true);
+                mNavigationDrawer.closeDrawer(GravityCompat.START);
+                return false;
+            }
+        });
     }
 }
