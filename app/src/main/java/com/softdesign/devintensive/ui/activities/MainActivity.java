9084042d7ddaa09607c5a2d83737ccf124be3a6c
Mihail Reznichenko <mihail.reznichenko.98@mail.ru>
2016-06-28 15:27:55 +0600

    add Navigation Drawer

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -1,20 +1,41 @@
 package com.softdesign.devintensive.ui.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.utils.ConstantManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener{
     private static final String TAG = ConstantManager.TAG_PREFIX + "MainActivity";
+
+    private ImageView mCallImg;
+    private CoordinatorLayout mCoordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "OnCreate");
+
+        mCallImg = (ImageView)findViewById(R.id.call_img);
+        mCoordinatorLayout = (CoordinatorLayout)findViewById(R.id.main_coordinator_container);
+
+        mCallImg.setOnClickListener(this);
+
+        if (savedInstanceState == null){
+            //активити запускается впервые
+
+        } else{
+            //активити уже создавалась
+
+        }
     }
 
     @Override
@@ -51,5 +72,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onRestart() {
         super.onRestart();
         Log.d(TAG, "OnRestart");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.call_img:
+                //showProgress();
+                break;
+        }
+    }
+
+    private void showSnackBar(String message){
+        Snackbar.make(mCoordinatorLayout, message, Snackbar.LENGTH_LONG).show();
     }
 }
